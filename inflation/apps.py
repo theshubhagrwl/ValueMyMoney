4d179fba38e145d1a206faf8764850d91f1d4fb2
Shubh Agrawal <shubhagrawal1280@gmail.com>
2020-06-17 17:16:18 +0530

    initial commit

---
+++ inflation/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class InflationConfig(AppConfig):
+    name = 'inflation'
---
+++ inflation_analyser/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for inflation_analyser project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inflation_analyser.settings')
+
+application = get_asgi_application()

644953ace13d718e0a7798c6d76b737f9e45cdd7
Shubh Agrawal <shubhagrawal1280@gmail.com>
2020-06-18 23:37:11 +0530

    added chartjs

--- inflation/views.py
+++ inflation/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
 import pandas as pd
 import matplotlib.pyplot as plt
+import random as re
 
 
 def Home(request):
-    # df = pd.read_csv('static/india-cpi.csv')
-    # context = {
-    #     'df': df.to_html(),
-    #     'head': df.head().to_html(),
-    # }
-    # return render(request, 'inflation/index.html', context)
     return render(request, 'inflation/index.html')
 
 
 def graph(request):
+
+    def getRandomColour():
+        color = 'hsl(' + str(re.randint(1, 360)) + ", 100%, 75%)"
+        return color
+
     start_date = request.GET.get('start_date')
     end_date = request.GET.get('end_date')
     amount = request.GET.get('amount')
 
     year_list = []
+    color_list = []
     for i in range(int(start_date), int(end_date)+1):
         year_list.append(i)
-    # print(year_list)
+        color_list.append(getRandomColour())
     principal_amt = float(amount)
 
     start_date = start_date + "-12-" + "31"
@@ -32,7 +33,6 @@ def graph(request):
     df = df.set_index("date")
 
     inflation_list = list(df.loc[start_date:end_date, " inflation-rate"])
-    print(inflation_list)
 
     list_for_p = []
     for item in inflation_list:
@@ -52,5 +52,6 @@ def graph(request):
         'year_list': year_list,
         'zipped_data': zipped_date,
         'inflation_list': inflation_list,
+        'color_list': color_list,
     }
     return render(request, 'inflation/graph.html', context)
